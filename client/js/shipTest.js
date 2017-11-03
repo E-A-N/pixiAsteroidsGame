bb5105e3231ef46300647ee8132ac564fdc8e4d1
Cyborg_Ean <ean.director@gmail.com>
2017-11-02 17:19:14 -0700

    setting up and testing basic api tests and game logic

---
+++ client/js/shipTest.js
@@ -0,0 +1,30 @@
+//setup game constants
+var _container      = PIXI.container;
+var _renderEngine   = PIXI.autoDetectRenderer;
+var _loader         = PIXI.loader;
+var _resources      = PIXI.resources;
+var _sprite         = PIXI.Sprite;
+var _gameWidth      = 256;
+var _gameHeight     = 256;
+var _imgRoot       = "../img/";
+
+//Create a stage and add it to DOM
+var stage = new _container();
+var renderer = _renderEngine(_gameWidth, _gameHeight) //draw the stage
+document.body.appendChild(renderer.view);
+
+var preload = function(){
+
+}
+
+var createShip = function(x,y,resource){
+     //var 
+}
+
+
+//load graphical assets
+_loader
+    .add(_imgRoot + "fireball.png")
+    .add(_imgRoot + "ship1.png")
+    .add(_imgRoot + "ship2.png")
+    .load()
