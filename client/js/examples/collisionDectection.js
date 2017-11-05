d52d8c045ecce05f2bba78235f968e680219e131
Cyborg_Ean <ean.director@gmail.com>
2017-11-05 07:19:08 -0800

    Added collision detection method to gameObjects

--- client/js/examples/collisionDectection.js
+++ client/js/examples/collisionDectection.js
@@ -1,50 +1,34 @@
-function hitTestRectangle(r1, r2) {
+function spriteCollision(spr1, spr2) {
 
-  //Define the variables we'll need to calculate
-  var hit, combinedHalfWidths, combinedHalfHeights, vx, vy;
+    //Define the variables we'll need to calculate
+    var hit, combinedHalfWidths, combinedHalfHeights, vx, vy;
 
-  //hit will determine whether there's a collision
-  hit = false;
-
-  //Find the center points of each sprite
-  r1.centerX = r1.x + r1.width / 2;
-  r1.centerY = r1.y + r1.height / 2;
-  r2.centerX = r2.x + r2.width / 2;
-  r2.centerY = r2.y + r2.height / 2;
-
-  //Find the half-widths and half-heights of each sprite
-  r1.halfWidth = r1.width / 2;
-  r1.halfHeight = r1.height / 2;
-  r2.halfWidth = r2.width / 2;
-  r2.halfHeight = r2.height / 2;
-
-  //Calculate the distance vector between the sprites
-  vx = r1.centerX - r2.centerX;
-  vy = r1.centerY - r2.centerY;
-
-  //Figure out the combined half-widths and half-heights
-  combinedHalfWidths = r1.halfWidth + r2.halfWidth;
-  combinedHalfHeights = r1.halfHeight + r2.halfHeight;
-
-  //Check for a collision on the x axis
-  if (Math.abs(vx) < combinedHalfWidths) {
-
-    //A collision might be occuring. Check for a collision on the y axis
-    if (Math.abs(vy) < combinedHalfHeights) {
-
-      //There's definitely a collision happening
-      hit = true;
-    } else {
-
-      //There's no collision on the y axis
-      hit = false;
-    }
-  } else {
-
-    //There's no collision on the x axis
+    //hit will determine whether there's a collision
     hit = false;
-  }
 
-  //`hit` will be either `true` or `false`
-  return hit;
+    //Find the center points of each sprite
+    spr1.centerX = spr1.x + spr1.width / 2;
+    spr1.centerY = spr1.y + spr1.height / 2;
+    spr2.centerX = spr2.x + spr2.width / 2;
+    spr2.centerY = spr2.y + spr2.height / 2;
+
+    //Find the half-widths and half-heights of each sprite
+    spr1.halfWidth = spr1.width / 2;
+    spr1.halfHeight = spr1.height / 2;
+    spr2.halfWidth = spr2.width / 2;
+    spr2.halfHeight = spr2.height / 2;
+
+    //Calculate the distance vector between the sprites
+    vx = spr1.centerX - spr2.centerX;
+    vy = spr1.centerY - spr2.centerY;
+
+    //Figure out the combined half-widths and half-heights
+    combinedHalfWidths = spr1.halfWidth + spr2.halfWidth;
+    combinedHalfHeights = spr1.halfHeight + spr2.halfHeight;
+
+    var horizontalCollision = Math.abs(vx) < combinedHalfWidths;
+    var verticalCollision   = Math.abs(vy) < combinedHalfHeights;
+    var hit = horizontalCollision && verticalCollision;
+
+    return hit;
 };
