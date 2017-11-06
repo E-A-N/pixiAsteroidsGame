10c4b2c3b9ec54eb8b57e95341b026e02c452d6b
Cyborg_Ean <ean.director@gmail.com>
2017-11-05 21:29:17 -0800

    variable size asteroids implemented, now working on rock splitting event

--- client/js/gameObjects/asteroidRock.js
+++ client/js/gameObjects/asteroidRock.js
@@ -8,6 +8,7 @@ var asteroidRock = function(spr){
     spr.acceleration = 1.90;
     spr.spd = 5;
     spr.friction = .99; //speed at which spr will gradually slowdown
+
     //choose random direction to spin
     spr.rotateDirection = Math.round(Math.random()) ? 1 : -1;
 
@@ -18,19 +19,57 @@ var asteroidRock = function(spr){
 
     spr.create = function(){
         switch (spr.sizeState){
-            case "med":
-            case "medium":
-                spr.vx = Math.floor(Math.random() * 5) * spr.rotateDirection;
-                spr.vy = Math.floor(Math.random() * 5) * spr.rotateDirection;
-                break;
 
             case "small":
-                spr.vx = Math.floor(Math.random() * 10) * spr.rotateDirection;
-                spr.vy = Math.floor(Math.random() * 10) * spr.rotateDirection;
+                spr.width  /= 5;
+                spr.height /= 5;
+                spr.turnSpd = Math.floor(Math.random() * 10)/ 1000;
+                spr.vx = (Math.floor(Math.random() * 8)/8) * spr.rotateDirection;
+                spr.vy = (Math.floor(Math.random() * 8)/8) * spr.rotateDirection;
+                break;
+
+            case "medium":
+                spr.width  /= 2;
+                spr.height /= 2;
+                spr.turnSpd = Math.floor(Math.random() * 50)/ 1000;
+                spr.vx = (Math.floor(Math.random() * 5)/15) * spr.rotateDirection;
+                spr.vy = (Math.floor(Math.random() * 5)/15) * spr.rotateDirection;
                 break;
 
             default:
             case "big":
+            spr.width  *= 1.2;
+            spr.height *= 1.2;
+                spr.turnSpd = Math.floor(Math.random() * 100)/ 10000;
+                spr.vx = (Math.floor(Math.random() * 10)/20) * spr.rotateDirection;
+                spr.vy = (Math.floor(Math.random() * 10)/20) * spr.rotateDirection;
+                break;
+        }
+    }
+
+    spr.explode = function(){
+        switch (spr.sizeState){
+
+            case "small":
+                spr.width  /= 5;
+                spr.height /= 5;
+                spr.turnSpd = Math.floor(Math.random() * 10)/ 1000;
+                spr.vx = (Math.floor(Math.random() * 8)/8) * spr.rotateDirection;
+                spr.vy = (Math.floor(Math.random() * 8)/8) * spr.rotateDirection;
+                break;
+
+            case "medium":
+                spr.width  /= 2;
+                spr.height /= 2;
+                spr.turnSpd = Math.floor(Math.random() * 50)/ 1000;
+                spr.vx = (Math.floor(Math.random() * 5)/15) * spr.rotateDirection;
+                spr.vy = (Math.floor(Math.random() * 5)/15) * spr.rotateDirection;
+                break;
+
+            default:
+            case "big":
+            spr.width  *= 1.2;
+            spr.height *= 1.2;
                 spr.turnSpd = Math.floor(Math.random() * 100)/ 10000;
                 spr.vx = (Math.floor(Math.random() * 10)/20) * spr.rotateDirection;
                 spr.vy = (Math.floor(Math.random() * 10)/20) * spr.rotateDirection;
