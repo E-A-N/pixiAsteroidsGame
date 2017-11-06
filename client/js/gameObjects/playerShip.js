64b7b07c93ef95613c9dfe6d7aef14811d1f2656
Cyborg_Ean <ean.director@gmail.com>
2017-11-05 20:37:35 -0800

    callback function and parameters work for collision detection method! Fully implementedgit add -A

--- client/js/gameObjects/playerShip.js
+++ client/js/gameObjects/playerShip.js
@@ -8,22 +8,26 @@ var playerShip = function(spr){
     spr.turnSpd = .05; //speed at which ship rotates
     spr.friction = .997; //speed at which ship will gradually slowdown
 
-    spr.name = "playerShip"
+    spr.name = "playerShip";
+
+    //This method is for debugging
+    spr.debugMsg = function (msg){
+        if(spr.debug){
+            spr.debug.text = "DEBUG: " + msg;
+        }
+    }
 
     spr.update = function(){
         playerControls(spr);
         //Check for any collisions
-        // spr.singleCollisionCheck(_gameMaster.spriteList, function(self, spr2){
-        //     console.log(spr2.id);
-        // });
+        spr.singleCollisionCheck(_gameMaster.spriteList, function(self, spr2){
+            var msg = self.id +" is colliding with "+ spr2.id;
+            spr.debugMsg(msg);
+        });
         spr.screenWrap();
         spr.x += spr.vx;
         spr.y += spr.vy;
 
-        if(spr.debug){
-            var eSpr = spr.singleCollisionCheck(_gameMaster.spriteList);
-            var msg = "colliding with: " + eSpr;
-            spr.debug.text = "DEBUG: " + msg;
-        }
+
     }
 }
