37522ca02f23266e3ff1fc23cb34bd4d68577f9b
Cyborg_Ean <ean.director@gmail.com>
2017-12-04 14:50:53 -0800

    working on audio, learning howler.js api

---
+++ client/js/utilities/sound.js
@@ -0,0 +1,7 @@
+var src = [
+    "/client/audio/bangSmall.wav",
+    "/client/audio/bangMedium.wav",
+    "/client/audio/bangLarge.wav",
+    "/client/audio/fire.wav",
+    "/client/audio/thrust.wav",
+];
