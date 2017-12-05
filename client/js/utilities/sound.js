bb5d6443f7373558039ce50a322246cbf9602a37
Cyborg_Ean <ean.director@gmail.com>
2017-12-05 13:26:05 -0800

    implementing and testing howler js sound api

--- client/js/utilities/sound.js
+++ client/js/utilities/sound.js
@@ -1,7 +1,29 @@
-var src = [
-    "/client/audio/bangSmall.wav",
-    "/client/audio/bangMedium.wav",
-    "/client/audio/bangLarge.wav",
-    "/client/audio/fire.wav",
-    "/client/audio/thrust.wav",
-];
+var Sound = function(src){
+    this.src = src || [
+        "/client/audio/bangSmall.wav",
+        "/client/audio/bangMedium.wav",
+        "/client/audio/bangLarge.wav",
+        "/client/audio/fire.wav",
+        "/client/audio/thrust.wav",
+    ];
+
+    this.soundCollection = [];
+};
+
+/*
+*    This method is to be used for traversal through source collection
+*/
+Sound.prototype._setupSound = function(x){
+    var config = {
+        src: x,
+        autoplay: false,
+        loop: false,
+        volume: 0.5,
+    }
+    return new Howl(config);
+}
+
+Sound.prototype.init = function(){
+    var method = this._setupSound;
+    this.soundCollection = this.src.map(method);
+};
