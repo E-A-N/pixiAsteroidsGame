b49b202029d6b06cc5daf459fb433939981aa7fa
Cyborg_Ean <ean.director@gmail.com>
2017-12-07 07:14:11 -0800

    laying out score modifyer methods

--- client/js/gameObjects/scoreModifier.js
+++ client/js/gameObjects/scoreModifier.js
@@ -8,6 +8,21 @@ ScoreModifyer.prototype.setBonus = function(){
     this.bonus = this.mod * this.multiplyer;
 };
 
+
+/**
+*    @returns {number} - total value to add to score
+*/
 ScoreModifyer.prototype.getBonus = function(){
     return this.bonus;
 }
+
+/**
+*   This function adds to score bonus based travel distance of bullet
+*/
+ScoreModifyer.prototype.setModifier = function(){}
+
+/**
+*    This method increases multiplyer field based on combo counter
+    TODO: add a combo counter system
+*/
+ScoreModifyer.prototype.setMultiplyer = function(){}
