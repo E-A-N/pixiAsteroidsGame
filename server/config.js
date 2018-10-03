d985abcdcfa2606755ad9340edc769fc934b707c
Cyborg_Ean <ean.director@gmail.com>
2018-10-03 07:08:34 -0700

    updated config withbasic attributes

--- server/config.js
+++ server/config.js
@@ -3,6 +3,16 @@ module.exports = {
         currentRoom: "gameRoom",
         successfulJoin: "joinGameSuccess",
         playerAttributes: {
-
+            acceleration: 0.5,
+            speed: 5,
+            turnSpeed: .05,
+            friction: 0.997,
+            canShoot: true,
+            coolingDown: false,
+            coolDownPeriod: 10,
+            coolDownTime: 0,
+            name: "",
+            respawnTime: 200,
         }
+        //eanDebug after image effect should be implemented on client side
 }
